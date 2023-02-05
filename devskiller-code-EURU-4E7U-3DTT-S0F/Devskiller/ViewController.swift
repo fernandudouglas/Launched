fee8a539c2b3469f8751dd5c261a13e82d35c1e7
Fernando Douglas C Vieira <fdouglas.developer@gmail.com>
2023-02-05 16:47:11 -0300

    viewcode setup

---
+++ devskiller-code-EURU-4E7U-3DTT-S0F/Devskiller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Devskiller
+//
+//  Created by Ivo Silva on 18/09/2020.
+//  Copyright © 2020 Mindera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
